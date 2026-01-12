53d13a926ba3b41fe21b2866052d9bb822b617b8
jay.m@hbsoftweb.in <jay.m@hbsoftweb.in>
2026-01-12 17:22:08 +0530

    added thank-you.php

--- thank-you.php
+++ thank-you.php
@@ -1,11 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Thank You</title>
+    <title>Thank You - Dr Nidhi Shah</title>
+    <link rel="shortcut icon" href="./assets/fevicon/fevicon.png" type="image/x-icon">
+
+    <link rel="stylesheet" href="./css/thanks.css">
+    <link rel="stylesheet" href="./css/hero.css">
+    <link rel="stylesheet" href="./css/about.css">
+    <link rel="stylesheet" href="./css/index.css">
 </head>
+
 <body>
-    <h1>Thanks</h1>
+    <section class="thank-you-section">
+        <div class="container">
+            <img src="./assets/images/Dr nidhi shah logo.svg" alt="" class="thank-you-logo">
+            <h1 class="thanks-title">Thank You !</h1>
+            <span class="thanks-description">Thank You for Contacting Us We will Get Back to You Soon!</span>
+            <a href="./" class="slide-main-button counter-button">
+                <p class="text">Go To Home</p>
+                <span class="hero-slide-button-icon-holder flex">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 11">
+                        <path
+                            d="M15.1,4.9v1.3h0h-0.6c-1.1,0-2.1,0.5-2.9,1.2c-0.8,0.8-1.2,1.8-1.2,2.9v0.6H9.1v-0.6c0-1.4,0.6-2.7,1.6-3.7 c0.1-0.1,0.3-0.2,0.4-0.4H0.5V4.9h10.6c-0.1-0.1-0.3-0.2-0.4-0.4c-1-1-1.6-2.3-1.6-3.7V0.2h1.3v0.6c0,1.1,0.5,2.1,1.2,2.9 c0.8,0.8,1.8,1.2,2.9,1.2L15.1,4.9L15.1,4.9L15.1,4.9z">
+                        </path>
+                    </svg>
+                </span>
+            </a>
+        </div>
+    </section>
 </body>
+
 </html>
