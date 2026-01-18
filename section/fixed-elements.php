27ded76cf2c4dbb811b545048e113295d3677202
jay.m@hbsoftweb.in <jay.m@hbsoftweb.in>
2026-01-18 18:35:08 +0530

    .

--- section/fixed-elements.php
+++ section/fixed-elements.php
@@ -3,16 +3,21 @@
 ?>
 
 <!-- WhatsApp Icon -->
-<a href="https://wa.me/918080858170" target="_blank" rel="noopener noreferrer" class="whatsapp-icon" title="Chat on WhatsApp">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-        <path d="M17.507 14.307l-.611-1.418a1.44 1.44 0 0 0-1.966-.727l-.882.539a11.342 11.342 0 0 1-1.279-.369 11.342 11.342 0 0 1-1.115-1.059l.539-.882a1.44 1.44 0 0 0-.727-1.966l-1.418-.611a1.44 1.44 0 0 0-1.972.845l-.231.913a1.44 1.44 0 0 0 1.234 1.85c.59.082 1.177.203 1.747.369a13.114 13.114 0 0 0 1.732 1.105l-.363.594a1.44 1.44 0 0 0-.227 1.404l.611 1.418a1.44 1.44 0 0 0 1.966.727l.882-.539a11.342 11.342 0 0 1 1.279.369 11.342 11.342 0 0 1 1.115 1.059l-.539.882a1.44 1.44 0 0 0 .727 1.966l1.418.611a1.44 1.44 0 0 0 1.972-.845l.231-.913a1.44 1.44 0 0 0-1.234-1.85c-.59-.082-1.177-.203-1.747-.369a13.114 13.114 0 0 0-1.732-1.105l.363-.594a1.44 1.44 0 0 0 .227-1.404M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2"/>
+<a href="https://wa.me/918080858170" target="_blank" rel="noopener noreferrer" class="whatsapp-icon"
+    title="Chat on WhatsApp">
+    <svg xmlns="http://www.w3.org/2000/svg" shape-rendering="geometricPrecision" text-rendering="geometricPrecision"
+        image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 509 511.514">
+        <path fill="#fff"
+            d="M434.762 74.334C387.553 26.81 323.245 0 256.236 0h-.768C115.795.001 2.121 113.696 2.121 253.456l.001.015a253.516 253.516 0 0033.942 126.671L0 511.514l134.373-35.269a253.416 253.416 0 00121.052 30.9h.003.053C395.472 507.145 509 393.616 509 253.626c0-67.225-26.742-131.727-74.252-179.237l.014-.055zM255.555 464.453c-37.753 0-74.861-10.22-107.293-29.479l-7.72-4.602-79.741 20.889 21.207-77.726-4.984-7.975c-21.147-33.606-32.415-72.584-32.415-112.308 0-116.371 94.372-210.743 210.741-210.743 56.011 0 109.758 22.307 149.277 61.98a210.93 210.93 0 0161.744 149.095c0 116.44-94.403 210.869-210.844 210.869h.028zm115.583-157.914c-6.363-3.202-37.474-18.472-43.243-20.593-5.769-2.121-10.01-3.202-14.315 3.203-4.305 6.404-16.373 20.593-20.063 24.855-3.69 4.263-7.401 4.815-13.679 1.612-6.278-3.202-26.786-9.883-50.899-31.472a192.748 192.748 0 01-35.411-43.867c-3.712-6.363-.404-9.777 2.82-12.873 3.224-3.096 6.363-7.381 9.48-11.092a41.58 41.58 0 006.357-10.597 11.678 11.678 0 00-.508-11.09c-1.718-3.18-14.444-34.357-19.534-47.06-5.09-12.703-10.37-10.603-14.272-10.901-3.902-.297-7.911-.19-12.089-.19a23.322 23.322 0 00-16.964 7.911c-5.707 6.298-22.1 21.673-22.1 52.849s22.671 61.249 25.852 65.532c3.182 4.284 44.663 68.227 108.288 95.649 15.099 6.489 26.891 10.392 36.053 13.403a87.504 87.504 0 0025.216 3.718c4.905 0 9.82-.416 14.65-1.237 12.174-1.782 37.453-15.291 42.776-30.073s5.303-27.57 3.711-30.093c-1.591-2.524-5.704-4.369-12.088-7.615l-.038.021z" />
     </svg>
 </a>
 
 <!-- Scroll to Top Button -->
 <button class="scroll-to-top" id="scrollToTopBtn" title="Go to top">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-        <path d="M7 14l5-5 5 5z"/>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+        stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+        class="lucide lucide-chevron-up-icon lucide-chevron-up">
+        <path d="m18 15-6-6-6 6" />
     </svg>
 </button>
 
@@ -21,7 +26,7 @@
     const scrollToTopBtn = document.getElementById('scrollToTopBtn');
 
     // Show/hide button based on scroll position
-    window.addEventListener('scroll', function() {
+    window.addEventListener('scroll', function () {
         if (window.pageYOffset > 300) {
             scrollToTopBtn.classList.add('show');
         } else {
@@ -30,7 +35,7 @@
     });
 
     // Scroll to top on button click
-    scrollToTopBtn.addEventListener('click', function() {
+    scrollToTopBtn.addEventListener('click', function () {
         window.scrollTo({
             top: 0,
             behavior: 'smooth'
